385fb70de1e518a540093d5aaa314df48a40e966
kemalcanbora <kemalcanbora@gmail.com>
2019-06-15 23:07:23 +0300

    rev

--- setup.py
+++ setup.py
@@ -12,14 +12,22 @@ def get_long_description() -> str:
         return ld_file.read()
 
 
+def get_version() -> str:
+    black_py = CURRENT_DIR / "linkedin_api/__init__.py"
+    _version_re = re.compile(r"__version__\s+=\s+(?P<version>.*)")
+    with open(black_py, "r", encoding="utf8") as f:
+        match = _version_re.search(f.read())
+        version = match.group("version") if match is not None else '"unknown"'
+    return str(ast.literal_eval(version))
+
 
 setuptools.setup(
     name="linkedin_api",
-    version="1.1.1.1",
+    version=get_version(),
     author="Tom Quirk",
     author_email="tomquirkacc@gmail.com",
     description="Python wrapper for the Linkedin API",
-    long_description="sdf",
+    long_description=get_long_description(),
     long_description_content_type="text/markdown",
     url="https://github.com/tomquirk/linkedin-api",
     license="MIT",

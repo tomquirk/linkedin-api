3848e7c5ca361b39cc6f0eee35c89df36a4960e0
Aecio Cavalcanti <aecio.rcv@gmail.com>
2020-03-19 13:51:24 -0400

    black

--- linkedin_api/linkedin.py
+++ linkedin_api/linkedin.py
@@ -522,8 +522,11 @@ class Linkedin(object):
         Return job posting details for a given [job_urn_id], e.g: 1677638156
         """
 
-        params = {'decorationId': 'com.linkedin.voyager.deco.jobs.web.shared.WebFullJobPosting-39', 'topN': '1',
-                  'topNRequestedFlavors': 'List(IN_NETWORK,COMPANY_RECRUIT,SCHOOL_RECRUIT,HIDDEN_GEM)'}
+        params = {
+            "decorationId": "com.linkedin.voyager.deco.jobs.web.shared.WebFullJobPosting-39",
+            "topN": "1",
+            "topNRequestedFlavors": "List(IN_NETWORK,COMPANY_RECRUIT,SCHOOL_RECRUIT,HIDDEN_GEM)",
+        }
 
         res = self._fetch(f"/jobs/jobPostings/{job_urn_id}", params=params)
 

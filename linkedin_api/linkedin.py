8ab63b0493517a82fa1c88162e3cf0d32360f838
Aecio Cavalcanti <aecio.rcv@gmail.com>
2020-03-19 13:48:11 -0400

    change job_id to job_urn_id

--- linkedin_api/linkedin.py
+++ linkedin_api/linkedin.py
@@ -517,15 +517,15 @@ class Linkedin(object):
 
         return company
 
-    def get_job_posting(self, job_id):
+    def get_job_posting(self, job_urn_id):
         """
-        Return job posting details for a given [job_id], e.g: 1677638156
+        Return job posting details for a given [job_urn_id], e.g: 1677638156
         """
 
         params = {'decorationId': 'com.linkedin.voyager.deco.jobs.web.shared.WebFullJobPosting-39', 'topN': '1',
                   'topNRequestedFlavors': 'List(IN_NETWORK,COMPANY_RECRUIT,SCHOOL_RECRUIT,HIDDEN_GEM)'}
 
-        res = self._fetch(f"/jobs/jobPostings/{job_id}", params=params)
+        res = self._fetch(f"/jobs/jobPostings/{job_urn_id}", params=params)
 
         data = res.json()
 

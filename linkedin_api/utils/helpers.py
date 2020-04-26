ec242391830c6c5e8629efee4b95408ea2a2aaf9
Rithwik Ediga Lakhamsani <ricky.ediga@gmail.com>
2020-04-26 15:56:24 -0700

    Format with Black

--- linkedin_api/utils/helpers.py
+++ linkedin_api/utils/helpers.py
@@ -1,5 +1,6 @@
 import json
 
+
 def get_id_from_urn(urn):
     """
     Return the ID of a given Linkedin URN.
@@ -8,22 +9,24 @@ def get_id_from_urn(urn):
     """
     return urn.split(":")[3]
 
+
 def write_to_file(input_data, file_name):
     """
     Writes an input (whether or not JSON) to an output file
     """
     f = open(file_name, "w")
-    if(type(input_data) is dict):
+    if type(input_data) is dict:
         input_data = json.dumps(input_data)
     f.write(input_data)
     f.close()
+
 
 def append_to_file(input_data, file_name):
     """
     Appends an input (whether or not JSON) to an output file
     """
     f = open(file_name, "a")
-    if(type(input_data) is dict):
+    if type(input_data) is dict:
         input_data = json.dumps(input_data)
     f.write(input_data)
     f.close()

22700f8167ee579ca8b1d92900a2b17e0792c93f
Tom Quirk <tquirk@gitlab.com>
2020-08-25 22:20:22 +0930

    black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,5 +52,5 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-master_doc = 'index'
+master_doc = "index"
 html_static_path = []

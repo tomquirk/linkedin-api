9afc3a60231ee5b7ebe2dd4bdb876705b8d8174d
javier.legido <javier.legido@kedu.coop>
2020-08-31 12:37:53 +0200

    Implemented function to return a list with dicts corresponding to the posts in the personal feed, sort by 'Recent'

--- linkedin_api/linkedin.py
+++ linkedin_api/linkedin.py
@@ -9,7 +9,9 @@ from urllib.parse import urlencode, quote
 
 from linkedin_api.client import Client
 from linkedin_api.utils.helpers import get_id_from_urn, \
-    get_urn_from_raw_group_update
+    get_urn_from_raw_group_update, get_update_author_name, get_update_old, \
+    get_update_content, get_update_author_profile, get_update_url, \
+    append_update_post_field_to_posts_list
 
 logger = logging.getLogger(__name__)
 
@@ -1224,17 +1226,23 @@ class Linkedin(object):
             )
         return results
 
-    def get_list_urn_feed_updates(self, limit = -1, offset = 0):
+    def get_feed_updates(self, limit = -1, offset = 0, is_skip_promoted = True):
         """Get a list of URNs from feed sorted by 'Recent'
 
         :param limit: Maximum length of the returned list, defaults to -1 (no limit)
         :type limit: int, optional
         :param offset: Index to start searching from
         :type offset: int, optional
+        :param is_skip_promoted: Exclude from the output promoted posts
+        :type is_skip_promoted: bool, optional
 
         :return: List of URNs
         :rtype: list
         """
+
+        _PROMOTED_STRING = 'Promoted'
+        _PROFILE_URL = f"{self.client.LINKEDIN_BASE_URL}/in/"
+
         count = Linkedin._MAX_SEARCH_COUNT
         if limit is None:
             limit = -1
@@ -1255,14 +1263,37 @@ class Linkedin(object):
                 params = params,
                 headers={"accept": "application/vnd.linkedin.normalized+json+2.1"},
             )
-            data = res.json()
+            l_data = res.json().get("included", {})
 
             new_elements = []
-            elements = data.get("data", {}).get("*elements", [])
-            for element in elements:
-                new_elements.append(
-                    get_urn_from_raw_group_update(element)
-                )
+            for i in l_data:
+                author_name = get_update_author_name(i)
+                if author_name:
+                    new_elements = append_update_post_field_to_posts_list(i,
+                        new_elements, 'author_name', author_name)
+
+                author_profile = get_update_author_profile(i,
+                    self.client.LINKEDIN_BASE_URL)
+                if author_profile:
+                    new_elements = append_update_post_field_to_posts_list(i,
+                        new_elements, 'author_profile', author_profile)
+
+                old = get_update_old(i)
+                if old:
+                    new_elements = append_update_post_field_to_posts_list(i,
+                        new_elements, 'old', old)
+
+                content = get_update_content(i)
+                if content:
+                    new_elements = append_update_post_field_to_posts_list(i,
+                        new_elements, 'content', content)
+
+                url = get_update_url(i,
+                    self.client.LINKEDIN_BASE_URL)
+                if url:
+                    new_elements = append_update_post_field_to_posts_list(i,
+                        new_elements, 'url', url)
+
             results.extend(new_elements)
 
             # break the loop if we're done searching
@@ -1279,3 +1310,4 @@ class Linkedin(object):
             self.logger.debug(f"results grew to {len(results)}")
 
         return results
+

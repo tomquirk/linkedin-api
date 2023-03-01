9a6f0507abd3641dd7b98c65fa704459ffb486fd
Farhan K <farhan.crazyeyes@gmail.com>
2023-03-01 15:11:02 -0500

    Update linkedin.py

--- linkedin_api/linkedin.py
+++ linkedin_api/linkedin.py
@@ -58,7 +58,6 @@ class Linkedin(object):
         self,
         username,
         password,
-        new_cookie,
         *,
         authenticate=True,
         refresh_cookies=False,
@@ -83,7 +82,7 @@ class Linkedin(object):
                 # `username` and `password` are not used at all in this case.
                 self.client._set_session_cookies(cookies)
             else:
-                self.client.authenticate(username, password, new_cookie)
+                self.client.authenticate(username, password)
 
     def _fetch(self, uri, evade=default_evade, base_request=False, **kwargs):
         """GET request to Linkedin API"""

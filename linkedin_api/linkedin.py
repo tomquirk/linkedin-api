cc8bdcfe034ae8913967d681fbeb3ac36b93a35e
Farhan K <farhan.crazyeyes@gmail.com>
2023-03-16 17:09:40 -0400

    Update linkedin.py

--- linkedin_api/linkedin.py
+++ linkedin_api/linkedin.py
@@ -619,7 +619,7 @@ class Linkedin(object):
         """
         # NOTE this still works for now, but will probably eventually have to be converted to
         # https://www.linkedin.com/voyager/api/identity/profiles/ACoAAAKT9JQBsH7LwKaE9Myay9WcX8OVGuDq9Uw
-        res = self._fetch(f"/identity/profiles/{public_id or urn_id}/profileView")
+        res = self._fetch(f"/identity/profiles/{public_id or urn_id}")
         try:
             data = res.json()
             print(data)

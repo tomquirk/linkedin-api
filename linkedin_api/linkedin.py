d205a3473a54ad0b425fcce05b881b74e3db7ca0
Peter W <34256109+pww217@users.noreply.github.com>
2024-03-14 06:49:07 -0500

    Remove API_BASE_URL from reply_invitation() POST URL (#352)

--- linkedin_api/linkedin.py
+++ linkedin_api/linkedin.py
@@ -1174,7 +1174,7 @@ class Linkedin(object):
         )
 
         res = self._post(
-            f"{self.client.API_BASE_URL}/relationships/invitations/{invitation_id}",
+            f"/relationships/invitations/{invitation_id}",
             params=params,
             data=payload,
         )

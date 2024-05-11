46b552db499defe180724dae5687b6ec20870e4e
Mehdi Rahimi <mehdii.rh78@gmail.com>
2024-05-11 04:40:01 +0330

    updated to v2.1.3

--- linkedin_api/__init__.py
+++ linkedin_api/__init__.py
@@ -5,7 +5,7 @@
 from .linkedin import Linkedin
 
 __title__ = "linkedin_api"
-__version__ = "2.1.2"
+__version__ = "2.1.3"
 __description__ = "Python Wrapper for the Linkedin API"
 
 __license__ = "MIT"

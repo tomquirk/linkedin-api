71154172edb39bceeaa3c5090e419e1168ade576
wangyan <yan.wang@rexpandco.com>
2024-09-25 18:49:34 +0800

    # bugfix: 修改包版本

--- linkedin_api/__init__.py
+++ linkedin_api/__init__.py
@@ -6,7 +6,7 @@ from .linkedin import Linkedin
 from .advanced_linkedin import AdvancedLinkedin
 
 __title__ = "linkedin_api"
-__version__ = "2.1.4"
+__version__ = "2.1.5"
 __description__ = "Python Wrapper for the Linkedin API"
 
 __license__ = "MIT"
